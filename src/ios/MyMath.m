610575a0c509d77779de1e5202a4c71a660e33cd
sk <skaih@qq.com>
2017-01-08 11:33:18 +0800

    1

---
+++ src/ios/MyMath.m
@@ -0,0 +1,20 @@
+/********* MyMath.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface MyMath : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation MyMath
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"标题" message:@"你好世界！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+        [alertview show];
+}
+
+@end
